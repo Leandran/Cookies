59e547f5c4bf1d5bdc5c84b7faf7f247ea4316b5
Leandran <leandranm@outlook.com>
2021-02-08 17:06:49 +0200

    update home controller and show_cookies.html.erb with a table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,16 @@
 class HomeController < ApplicationController
   def set_cookies
+    cookies[:username] = "Smith"
+    cookies[:customer_number] = "1234567890"
   end
 
   def show_cookies
+    @username = cookies[:username]
+    @customer_number = cookies[:customer_number]
   end
 
   def delete_cookies
+    cookies.delete :username
+    cookies.delete :customer_number
   end
 end
